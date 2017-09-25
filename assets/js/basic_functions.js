8616107115ddfd1dc7a8987a04d9005387e04fe8
Christian M. T. Takagi <tkgcmt@gmail.com>
2017-09-25 17:57:25 -0300

    botões e bg image
    a imagem não traz conteúdo relevante, é um aspecto estético apenas,
    portanto pode ser usado com css: background-image

--- assets/js/basic_functions.js
+++ assets/js/basic_functions.js
@@ -33,27 +33,3 @@ window.onclick = function (event) {
     }
 
 };
-
-/* jQuery */
-
-$(document).ready(function () {
-    $("#menu-toggle1").click(function () {
-        $(this).toggleClass("active");
-        $(".mobile-nav").toggleClass("show-mobile-nav");
-    });
-
-    $('html').click(function (event) {
-        var clickedOn = $(event.target);
-        /*if (!clickedOn.parents().andSelf().is("#myModal") && $("#myModal").css("display") != "none") {
-            console.log("Clicked outside myModal and");
-            $("#myModal").css("display", "none");
-        }*/
-        if (clickedOn.parents().andSelf().is("#menu-toggle1")) {
-            console.log("Clicked on", clickedOn[0], "inside the div");
-        } else {
-            console.log("Clicked outside the div");
-        }
-    });
-
-
-});
