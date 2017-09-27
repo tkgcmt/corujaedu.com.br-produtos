2cb9237d1ae756435560f6e01cc8d7f2e70ca5c9
Christian M. T. Takagi <tkgcmt@gmail.com>
2017-09-26 21:49:53 -0300

    organização das sessões e preenchendo conteúdo
    tomar cuidado com as margens - falta ajustar tamanho das imagens

--- assets/js/coruja_jQuery.js
+++ assets/js/coruja_jQuery.js
@@ -4,18 +4,37 @@ $(function () {
         $(".mobile-nav").toggleClass("show-mobile-nav");
     });
 
+    $("#log-in").click(function (e) {
+        $(".user-info").toggleClass("active");
+        if ($(".user-info").hasClass("active")) {
+            $(".logged-in").css("display", "block");
+            $(".not-logged-in").css("display", "none");
+        } else {
+            $(".not-logged-in").css("display", "block");
+            $(".logged-in").css("display", "none");
+        }
+    });
+    $("#log-out").click(function (e) {
+        $(".user-info").toggleClass("active");
+        if ($(".user-info").hasClass("active")) {
+            $(".logged-in").css("display", "block");
+            $(".not-logged-in").css("display", "none");
+        } else {
+            $(".not-logged-in").css("display", "block");
+            $(".logged-in").css("display", "none");
+        }
+    });
+
     $('html').click(function (event) {
         var clickedOn = $(event.target);
         /*if (!clickedOn.parents().andSelf().is("#myModal") && $("#myModal").css("display") != "none") {
             console.log("Clicked outside myModal and");
             $("#myModal").css("display", "none");
         }*/
-        if (clickedOn.parents().andSelf().is("#menu-toggle1")) {
+        if (clickedOn.parents().addBack().is("#menu-toggle1")) {
             console.log("Clicked on", clickedOn[0], "inside the div");
         } else {
             console.log("Clicked outside the div");
         }
     });
-
-
 });
